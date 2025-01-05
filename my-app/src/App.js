555b3b844f9773f287fa642145edc1c676e24017
ATHARV PANT <159293881+Atharv22-hue@users.noreply.github.com>
2025-01-05 19:44:22 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Common/header";
-import SubmitFeedback from "./components/User/SumitFeedback";
+import SubmitFeedback from "./components/User/SubmitFeedback";
 import ViewReviews from "./components/User/ViewReviews";
 import ApproveReview from "./components/admin/ApproveReview";
 
 const App = () => {
+  const styles = {
+    container: {
+      padding: "20px",
+      maxWidth: "1200px",
+      margin: "0 auto",
+    },
+  };
+
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<><SubmitFeedback /><ViewReviews /></>} />
-        <Route path="/admin" element={<ApproveReview />} />
-      </Routes>
+      <div style={styles.container}>
+        <Routes>
+          <Route path="/" element={<><SubmitFeedback /><ViewReviews /></>} />
+          <Route path="/admin" element={<ApproveReview />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
