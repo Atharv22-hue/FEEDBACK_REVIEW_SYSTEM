ce647641e614c04a43b5fa4a942a47290d9ed702
ATHARV PANT <159293881+Atharv22-hue@users.noreply.github.com>
2025-01-06 23:41:15 +0530

    Update ViewReviews.js

--- my-app/src/components/User/ViewReviews.js
+++ my-app/src/components/User/ViewReviews.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import SubmitFeedback from './components/User/SubmitFeedback';
+
 
 const ViewReviews = () => {
   const [reviews, setReviews] = useState([]);
