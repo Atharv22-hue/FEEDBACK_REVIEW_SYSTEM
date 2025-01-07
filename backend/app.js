3ac42391eadd39415f16ef32d8f66d0892319e23
ATHARV PANT <159293881+Atharv22-hue@users.noreply.github.com>
2025-01-07 22:25:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,9 @@ app.use(express.json());
 
 // Connect to MongoDB
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+
+app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${5000}`);
 });
 const cors = require("cors");
